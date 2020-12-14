02dfab1c54c85b4f45d65739257e22feb8963e8b
Rohit Garg <cryptolemy+git@gmail.com>
2020-12-14 07:56:05 -0800

    Changing how the canvas will resize

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { useEffect } from 'react';
 import paper from 'paper';
-import { makeStyles } from '@material-ui/core/styles';
 import gridImage from './svg/grid.svg';
-
-const useStyles = makeStyles({
-  canvas: {
-    width: '100%',
-    height: '100%',
-  }
-});
+import './App.css';
 
 function App() {
   const canvas = React.createRef();
-  const classes = useStyles();
 
   useEffect(() => {
     if (!canvas.current) return;
     paper.setup(canvas.current);
     paper.project.importSVG(gridImage);
+    paper.project.activeLayer.fitBounds(paper.view.bounds);
     paper.view.draw();
   }, [canvas]);
 
   return (
-    <canvas className={classes.canvas} ref={canvas} />
+    <canvas data-paper-resize={true} ref={canvas}></canvas>
   );
 }
 
