e96e6fe16f22800c0ed9e380f041dcea15b6f426
Rohit Garg <cryptolemy+git@gmail.com>
2020-12-13 22:44:05 -0800

    Adding PaperJS and Material-UI
    I've also added a basic grid in the background, though the mechanism
    to do this will probably change soon.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import paper from 'paper';
+import { makeStyles } from '@material-ui/core/styles';
+import gridImage from './svg/grid.svg';
+
+const useStyles = makeStyles({
+  canvas: {
+    width: '100%',
+    height: '100%',
+  }
+});
 
 function App() {
+  const canvas = React.createRef();
+  const classes = useStyles();
+
+  useEffect(() => {
+    if (!canvas.current) return;
+    paper.setup(canvas.current);
+    paper.project.importSVG(gridImage);
+    paper.view.draw();
+  }, [canvas]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <canvas className={classes.canvas} ref={canvas} />
   );
 }
 
